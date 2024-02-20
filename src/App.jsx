01af25535fac52c25a63e309abdd4fc4e0b497f2
garciamoramc <garciamoramc@gmail.com>
2024-02-20 16:43:53 +0100

    Adding styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import Board from './components/Board'
 
 export default function App() {
-
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
@@ -15,23 +14,10 @@ export default function App() {
     setCurrentMove(nextHistory.length - 1);
   }
 
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
+  function resetGame() {
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
   }
-
-  const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
 
   return (
     <div className="game">
@@ -39,9 +25,8 @@ export default function App() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <button onClick={resetGame}>Reset Game</button>
       </div>
     </div>
   );
 }
-
