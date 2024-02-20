37cbea73ca46b5ad7aa64bb037997dbd275bb71a
garciamoramc <garciamoramc@gmail.com>
2024-02-20 15:57:43 +0100

    First commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line react/prop-types
+export default function Square({ value, onSquareClick }) {
+    return (
+      <button className="square" onClick={onSquareClick}>
+        {value}
+      </button>
+    );
+  }
